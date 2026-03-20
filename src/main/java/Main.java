5f1e10b120c0c4397dd12d682e8062edc338952e
murthy0x05 <ppavankumarofficial@gmail.com>
2026-03-21 04:34:53 +0530

    feat(request header): added Request Header v2 Support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,39 +3,44 @@ import java.net.Socket;
 import java.io.*;
 
 public class Main {
-  public static void main(String[] args){
+    public static void main(String[] args) {
 
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    int port = 9092;
-    try {
-      serverSocket = new ServerSocket(port);
-      serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept();
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 9092;
+        try {
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+            clientSocket = serverSocket.accept();
 
-      InputStream is = clientSocket.getInputStream();
-      OutputStream os = clientSocket.getOutputStream();
+            InputStream is = clientSocket.getInputStream();
+            OutputStream os = clientSocket.getOutputStream();
 
-      DataInputStream dis = new DataInputStream(is);
-      DataOutputStream dos = new DataOutputStream(os);
+            DataInputStream dis = new DataInputStream(is);
+            DataOutputStream dos = new DataOutputStream(os);
 
-      dos.writeInt(0);
-      dos.writeInt(Integer.parseInt("00000007", 8));
+            int message_size = dis.readInt();
+            short request_api_version = dis.readShort();
+            short request_api_key = dis.readShort();
+            int correlation_id = dis.readInt();
 
-      dos.flush();
+            dos.writeInt(message_size);
+            dos.writeInt(correlation_id);
 
-      dis.close();
-      dos.close();
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
+            dos.flush();
+
+            dis.close();
+            dos.close();
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      }
     }
-  }
 }
